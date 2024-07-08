01bf00dc74a3d965b7f3d90807b17dba3ba0df4c
Lucas Cordeiro Araujo <lucascordeiroaraujo@gmail.com>
2024-07-08 01:26:45 -0300

    feat: create gyms controllers

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -2,13 +2,13 @@ import { FastifyInstance } from "fastify";
 
 import { verifyJwt } from "@/http/middlewares/verify-jwt";
 
-import { register } from "./controllers/register";
+import { register } from "./register";
 
-import { authenticate } from "@/http/controllers/authenticate";
+import { authenticate } from "@/http/controllers/users/authenticate";
 
-import { profile } from "@/http/controllers/profile";
+import { profile } from "@/http/controllers/users/profile";
 
-export async function appRoutes(app: FastifyInstance) {
+export async function usersRoutes(app: FastifyInstance) {
   app.post("/users", register);
 
   app.post("/sessions", authenticate);
